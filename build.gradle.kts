597841f99cf83bda2e82d4852f87c5ef623d905e
Bravos <baonq8205@gmail.com>
2026-01-04 16:34:04 +0700

    Fix library cannot use 3

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,14 +29,14 @@ repositories {
 dependencies {
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
-    implementation("org.springframework.boot:spring-boot-autoconfigure:4.0.1")
-    implementation("org.springframework:spring-context:7.0.2")
-    implementation("org.springframework:spring-web:7.0.2")
-    implementation("org.springframework:spring-webflux:7.0.2")
-    implementation("tools.jackson.core:jackson-databind:3.0.3")
-    implementation("tools.jackson.core:jackson-core:3.0.3")
-    implementation("org.springframework.kafka:spring-kafka:4.0.1")
-    implementation("org.springframework.data:spring-data-redis:4.0.1")
+    api("org.springframework.boot:spring-boot-autoconfigure:4.0.1")
+    api("org.springframework:spring-context:7.0.2")
+    api("org.springframework:spring-web:7.0.2")
+    api("org.springframework:spring-webflux:7.0.2")
+    api("tools.jackson.core:jackson-databind:3.0.3")
+    api("tools.jackson.core:jackson-core:3.0.3")
+    api("org.springframework.kafka:spring-kafka:4.0.1")
+    api("org.springframework.data:spring-data-redis:4.0.1")
 }
 
 publishing {
