006dff84092a23d2f25c8ad1e4f944a827a8f8ca
Bravos <baonq8205@gmail.com>
2026-01-19 10:06:06 +0700

    Bump version to 1.1.5 and update DefaultObjectMapper to use custom serializers

--- src/main/java/com/bravos/steak/commonutils/shared/helper/DefaultObjectMapper.java
+++ src/main/java/com/bravos/steak/commonutils/shared/helper/DefaultObjectMapper.java
@@ -3,6 +3,10 @@ package com.bravos.steak.commonutils.shared.helper;
 import tools.jackson.databind.DeserializationFeature;
 import tools.jackson.databind.ObjectMapper;
 import tools.jackson.databind.json.JsonMapper;
+import tools.jackson.databind.module.SimpleModule;
+import tools.jackson.databind.ser.std.ToStringSerializer;
+
+import java.math.BigInteger;
 
 /**
  * Provides a default ObjectMapper instance configured with specific settings<br>
@@ -11,10 +15,19 @@ import tools.jackson.databind.json.JsonMapper;
  */
 public class DefaultObjectMapper {
 
-  private static final ObjectMapper INSTANCE = JsonMapper.builder()
-      .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-      .findAndAddModules()
-      .build();
+  private static final ObjectMapper objectMapper;
+
+  static {
+    SimpleModule simpleModule = new SimpleModule();
+    simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+    simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
+    simpleModule.addSerializer(BigInteger.class, ToStringSerializer.instance);
+    objectMapper = JsonMapper.builder()
+        .addModule(simpleModule)
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        .build();
+  }
+
 
   /**
    * Get the default ObjectMapper instance.
@@ -22,7 +35,7 @@ public class DefaultObjectMapper {
    * @return the ObjectMapper instance
    */
   public static ObjectMapper get() {
-    return INSTANCE;
+    return objectMapper;
   }
 
 }
